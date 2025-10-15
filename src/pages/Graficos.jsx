66806823f21d2cb963c91c741d71f94ab899b078
Caio Sousa Mello <cmcaiomello@gmail.com>
2025-10-15 15:42:35 -0300

    cards

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -1,77 +1,52 @@
 import GraficoLinha from '../components/Graficolinha';
 import GraficoColuna from '../components/Graficocoluna';
-import '../components/Graficoglobal.scss'
+import '../components/Graficoglobal.scss';
+
 export default function Graficos() {
 
-const dadosMatriculas = [
-  {
-    _id: { courseName: "Administração", periodName: "Manhã" },
-    totalInscritos: 32,
-  },
-  {
-    _id: { courseName: "Administração", periodName: "Tarde " },
-    totalInscritos: 49,
-  },
-  {
-    _id: { courseName: "Automação Residencial e Robótica", periodName: "Manhã" },
-    totalInscritos: 4,
-  },
-  {
-    _id: { courseName: "Automação Residencial e Robótica", periodName: "Tarde " },
-    totalInscritos: 5,
-  },
-  {
-    _id: { courseName: "Informática", periodName: "Manhã" },
-    totalInscritos: 31,
-  },
-  {
-    _id: { courseName: "Informática", periodName: "Tarde " },
-    totalInscritos: 49,
-  },
-  {
-    _id: {
-      courseName: "Inglês Básico - Pré Intermediário (Diurno)",
-      periodName: "Manhã 1",
-    },
-    totalInscritos: 30,
-  },
-  {
-    _id: {
-      courseName: "Inglês Básico - Pré Intermediário (Diurno)",
-      periodName: "Manhã 2",
-    },
-    totalInscritos: 20,
-  },
-  {
-    _id: {
-      courseName: "Inglês Básico - Pré Intermediário (Diurno)",
-      periodName: "Tarde 1",
-    },
-    totalInscritos: 51,
-  },
-  {
-    _id: {
-      courseName: "Inglês Básico - Pré Intermediário (Diurno)",
-      periodName: "Tarde 2 ",
-    },
-    totalInscritos: 24,
-  },
-  {
-    _id: { courseName: "Inglês Intermediário (Sábados)", periodName: "Manhã" },
-    totalInscritos: 93,
-  },
-  {
-    _id: { courseName: "Inglês Pré Intermediário (Noturno)", periodName: "Noite" },
-    totalInscritos: 14,
-  },
-];
+  const dadosMatriculas = [
+    { _id: { courseName: "Administração", periodName: "Manhã" }, totalInscritos: 32 },
+    { _id: { courseName: "Administração", periodName: "Tarde " }, totalInscritos: 49 },
+    { _id: { courseName: "Automação Residencial e Robótica", periodName: "Manhã" }, totalInscritos: 4 },
+    { _id: { courseName: "Automação Residencial e Robótica", periodName: "Tarde " }, totalInscritos: 5 },
+    { _id: { courseName: "Informática", periodName: "Manhã" }, totalInscritos: 31 },
+    { _id: { courseName: "Informática", periodName: "Tarde " }, totalInscritos: 49 },
+    { _id: { courseName: "Inglês Básico - Pré Intermediário (Diurno)", periodName: "Manhã 1" }, totalInscritos: 30 },
+    { _id: { courseName: "Inglês Básico - Pré Intermediário (Diurno)", periodName: "Manhã 2" }, totalInscritos: 20 },
+    { _id: { courseName: "Inglês Básico - Pré Intermediário (Diurno)", periodName: "Tarde 1" }, totalInscritos: 51 },
+    { _id: { courseName: "Inglês Básico - Pré Intermediário (Diurno)", periodName: "Tarde 2 " }, totalInscritos: 24 },
+    { _id: { courseName: "Inglês Intermediário (Sábados)", periodName: "Manhã" }, totalInscritos: 93 },
+    { _id: { courseName: "Inglês Pré Intermediário (Noturno)", periodName: "Noite" }, totalInscritos: 14 },
+  ];
 
+  const dadosAgendamentos = [
+    { _id: "08/10/2025", totalAgendamentos: 1 },
+    { _id: "09/10/2025", totalAgendamentos: 3 },
+    { _id: "10/10/2025", totalAgendamentos: 7 },
+    { _id: "13/10/2025", totalAgendamentos: 28 },
+    { _id: "14/10/2025", totalAgendamentos: 55 },
+    { _id: "15/10/2025", totalAgendamentos: 120 },
+    { _id: "16/10/2025", totalAgendamentos: 54 },
+    { _id: "17/10/2025", totalAgendamentos: 70 },
+    { _id: "20/10/2025", totalAgendamentos: 53 },
+    { _id: "21/10/2025", totalAgendamentos: 37 },
+    { _id: "22/10/2025", totalAgendamentos: 28 },
+    { _id: "23/10/2025", totalAgendamentos: 29 },
+    { _id: "24/10/2025", totalAgendamentos: 43 },
+  ];
+
+
+  const totalMatriculas = dadosMatriculas.reduce((acc, item) => acc + item.totalInscritos, 0);
+  const totalAgendamentos = dadosAgendamentos.reduce((acc, item) => acc + item.totalAgendamentos, 0);
+
+ 
   return (
     <div className="bloco">
       <header>
-        <img src="src/components/frei.png" alt="" />
+        <img src="src/components/frei.png" alt="Logo Frei" />
         <h2>Dashboard Frei</h2>
       </header>
+
       <main>
         <div className="grafico-container">
           <h1 className="titulo">Dashboard de Matrículas</h1>
@@ -79,8 +54,22 @@ const dadosMatriculas = [
             Visualização dos dados de matrícula da instituição por curso e período
           </p>
 
+          <div className="cards-container">
+            <div className="card-res card-mat">
+              <h3>Total de Matrículas</h3>
+              <p className="valor">{totalMatriculas}</p>
+              <span className="desc">Soma de todos os cursos e períodos</span>
+            </div>
+
+            <div className="card-res card-agend">
+              <h3>Total de Agendamentos</h3>
+              <p className="valor">{totalAgendamentos}</p>
+              <span className="desc">Soma de todas as datas registradas</span>
+            </div>
+          </div>
+
           <div>
-            <GraficoLinha data={dadosMatriculas} />
+            <GraficoLinha data={dadosAgendamentos} />
             <GraficoColuna data={dadosMatriculas} />
           </div>
         </div>
