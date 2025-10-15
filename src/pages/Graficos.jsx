1e5f625f1210ed252a31cd0cf58973c165aaed94
Caio Sousa Mello <cmcaiomello@gmail.com>
2025-10-15 16:57:45 -0300

    .

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -1,6 +1,6 @@
 import Graficolinha from '../components/Graficolinha';
 import Graficocoluna from '../components/Graficocoluna';
-import '../components/graficoglobal.scss';
+import '../components/graficoGlobal.scss';
 
 export default function Graficos() {
 
